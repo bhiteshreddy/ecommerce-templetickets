44dad689fbd5ad419d2f22533cc27c0f8da0ac94
bhiteshreddy <131425882+bhiteshreddy@users.noreply.github.com>
2024-01-05 09:04:53 +0530

    Add files via upload

---
+++ authcart/views.py
@@ -0,0 +1,52 @@
+from tokenize import generate_tokens
+from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
+from django.contrib.auth.models import User
+from django.views.generic import View
+from django.contrib import messages
+
+from django.contrib.auth import authenticate,login,logout
+# Create your views here.
+def signup(request):
+    if request.method=="POST":
+        email=request.POST['email']
+        password=request.POST['pass1']
+        confirm_password=request.POST['pass2']
+        if password!=confirm_password:
+            messages.warning(request,"Password is Not Matching")
+            return render(request,"authentication/signup.html")                   
+        try:
+            if User.objects.get(username=email):
+                # return HttpResponse("email already exist")
+                messages.info(request,"Email is Taken")
+                return render(request,"authentication/signup.html")
+        except Exception as identifier:
+            pass
+        user = User.objects.create_user(email,email,password)
+        user.save()
+        
+    return render(request,"authentication/signup.html")
+
+def handlelogin(request):
+    if request.method=="POST":
+
+        username=request.POST['email']
+        userpassword=request.POST['pass1']
+        myuser=authenticate(username=username,password=userpassword)
+
+        if myuser is not None:
+            login(request,myuser)
+            messages.success(request,"Login Success")
+            return redirect('/')
+
+        else:
+            messages.error(request,"Invalid Credentials")
+            return redirect('/auth/login')
+    return render(request,"authentication/login.html")
+
+def handlelogout(request):
+    logout(request)
+    messages.info(request,"Logout Success")
+    return redirect('/auth/login')
+
+
